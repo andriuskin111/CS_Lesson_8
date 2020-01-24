1c9d10e74e80546f9f1227bb941a24fc0ac0f0c2
andriuskin111 <andriuskin@gmail.com>
2020-01-24 19:48:46 +0200

    prideta struktura Palme

--- NdSavarankiskasDarbasDu/NdSavarankiskasDarbasDu/Program.cs
+++ NdSavarankiskasDarbasDu/NdSavarankiskasDarbasDu/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PalmeStruct;
 
 namespace NdSavarankiskasDarbasDu
 {
@@ -10,6 +11,15 @@ namespace NdSavarankiskasDarbasDu
     {
         static void Main(string[] args)
         {
+            Palme palme;
+            palme.amzius = 8;
+            palme.vaisiuKiekis = 0;
+
+            palme.PridetiVienaMenesiAmziaus();
+
+            Console.WriteLine($"Vaisiu kiekis yra: {palme.VaisiuKiekis}");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ NdSavarankiskasDarbasDu/PalmeStruct/Palme.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalmeStruct
+{
+    public struct Palme
+    {
+        public int amzius;
+        public int vaisiuKiekis;
+
+        public int Amzius {
+            get { return amzius; }
+            set { }
+        }
+
+        public int VaisiuKiekis
+        {
+            get { return vaisiuKiekis; }
+            set { }
+        }
+
+        public void PridetiVienaMenesiAmziaus()
+        {
+            if ((Amzius + 1) > 5 && (Amzius + 1) < 12)
+            {
+                vaisiuKiekis =  (Amzius + 1 - 5) * 3;
+            }
+            else
+            {
+                vaisiuKiekis = 0;
+            }
+
+        }
+    }
+}
