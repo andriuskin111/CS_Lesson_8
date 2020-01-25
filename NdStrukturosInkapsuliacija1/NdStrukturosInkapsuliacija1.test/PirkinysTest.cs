d07702636405eff68212953c1de3b0f952810e71
andriuskin111 <andriuskin@gmail.com>
2020-01-25 11:55:54 +0200

    sukurtas projektas

---
+++ NdStrukturosInkapsuliacija1/NdStrukturosInkapsuliacija1.test/PirkinysTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NdStrukturosInkapsuliacija1.test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
