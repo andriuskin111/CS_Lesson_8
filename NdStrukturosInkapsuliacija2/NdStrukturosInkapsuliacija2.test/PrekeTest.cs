d1728c3310faf5d3cfe353aabc6d8813df72eba6
andriuskin111 <andriuskin@gmail.com>
2020-01-25 13:15:28 +0200

    prideta struktura

--- NdStrukturosInkapsuliacija2/NdStrukturosInkapsuliacija2.test/PrekeTest.cs
+++ NdStrukturosInkapsuliacija2/NdStrukturosInkapsuliacija2.test/PrekeTest.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrekeStruct;
 
 namespace NdStrukturosInkapsuliacija2.test
 {
     [TestClass]
-    public class UnitTest1
+    public class PrekeTest
     {
         [TestMethod]
         public void TestMethod1()
--- NdStrukturosInkapsuliacija2/PrekeStruct/Preke.cs
+++ NdStrukturosInkapsuliacija2/PrekeStruct/Preke.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace PrekeStruct
 {
-    public class Class1
+    public struct Preke
     {
+        private string _prekesPavadinimas;
+        private int _kiekis;
+        private double _kainaVieneto;
+
+        public Preke(string preke, int kiekis, double kaina)
+        {
+            _prekesPavadinimas = preke;
+            _kiekis = kiekis;
+            _kainaVieneto = kaina;               
+        }
+
+        public string PrekesPavadinimas
+        {
+            get { return _prekesPavadinimas; }
+            private set { }
+        }
+
+        public int Kiekis
+        {
+            get { return _kiekis; }
+            private set { }
+        }
     }
 }
