0f10cc129f23a2871f4b4a94a260632afda352aa
andriuskin111 <andriuskin@gmail.com>
2020-01-25 12:34:11 +0200

    sukurtas projektas

---
+++ NdStrukturosInkapsuliacija2/NdStrukturosInkapsuliacija2.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NdStrukturosInkapsuliacija2.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NdStrukturosInkapsuliacija2.test")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41ae8d7e-2c2b-467c-b538-3981d1735bca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
